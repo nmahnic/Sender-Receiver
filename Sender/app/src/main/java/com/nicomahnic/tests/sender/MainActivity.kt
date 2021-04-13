c4b0f093ddfaf46fe3c40e6dbfd47db79abd9ece
Nicolás Mahnic <nmahnic@pdssa.com>
2021-04-13 11:34:39 -0300

    simple version

--- Sender/app/src/main/java/com/nicomahnic/tests/sender/MainActivity.kt
+++ Sender/app/src/main/java/com/nicomahnic/tests/sender/MainActivity.kt
@@ -1,16 +1,18 @@
 package com.nicomahnic.tests.sender
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.gson.Gson
 
 class MainActivity : AppCompatActivity() {
 //    adb shell am start -n com.nicomahnic.tests.sender/com.nicomahnic.tests.sender.MainActivity
     private lateinit var texto : TextView
-    private lateinit var payment : Payment
+    private val REQUEST_CODE = 255
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,26 +22,42 @@ class MainActivity : AppCompatActivity() {
 
         val boton = findViewById<Button>(R.id.btnEnter)
         boton.setOnClickListener {
-            val transaction = DoPayment(
+            val transaction = Payment(
                     currency = "UYU",
                     currencyCode = 858,
                     transactionType = TransactionType.SALE,
                     amount = 12.50,
             )
-            payment = Payment.getInstance(this)
-            val newIntent = payment.launchIngpPinpad(transaction, getPackageManager())
-
-            startActivityForResult(newIntent.first,newIntent.second)
+            launchIngpPinpad(transaction, getPackageManager())
         }
+    }
+
+    private fun launchIngpPinpad(data: Payment, pm: PackageManager){
+        val sendData = Gson().toJson(data)
+        Log.d("NM", "1) Envio ${data}")
+        val sendIntent = Intent()
+        sendIntent.action = Intent.ACTION_SEND
+        sendIntent.putExtra(Intent.EXTRA_TEXT, sendData)
+        sendIntent.type = "text/plain"
+
+
+        val sharedIntent = CustomSenderIntent.create(pm,sendIntent,"com.nicomahnic.tests.receiver")
+        startActivityForResult(sharedIntent, REQUEST_CODE)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        payment.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CODE && resultCode == AppCompatActivity.RESULT_OK) {
+            val resultFromActivity2 = data!!.getStringExtra("result")
+            resultFromActivity2?.let{
+                val item = Gson().fromJson(it, PaymentResault::class.java)
+                Log.d("NM", "1) Respuesta ${item}")
+            }
+        }
     }
 }
 
-data class DoPayment(
+data class Payment(
         val currency: String,
         val currencyCode: Int,
         val transactionType: TransactionType,
